5b78bdc042378e7bbf833b7b33035b1736109f16
eremitik <39305876+eremitik@users.noreply.github.com>
2021-02-02 17:07:29 +0900

    Update helloworld.sol

--- helloworld.sol
+++ helloworld.sol
@@ -1,13 +1,14 @@
 pragma solidity ^0.6.0;
 
 contract A {
-    //1. call function of another contract
     address addressB;
     
+    //1. cannot simply set new variable as in JS, so need a function to set
     function setAddressB(address _addressB) external {
         addressB = _addressB;
     }
     
+    //2. once address B is set, can call function to execute contract B
     function callHelloWorld() external view returns (string memory) {
         B b = B(addressB);
         return b.helloWorld();
