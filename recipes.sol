885d65d0b77cd2a288d37cd7c902253b5401f7c8
eremitik <39305876+eremitik@users.noreply.github.com>
2021-02-11 11:22:36 +0900

    Update recipes.sol

--- recipes.sol
+++ recipes.sol
@@ -20,3 +20,33 @@ contract ContractB {
         //do something else
     }
 }
+
+
+//Factory-Child pattern
+pragma solidity ^0.6.0;                          
+                                                 
+contract Factory {                               
+    Child[] public children;                     
+    event ChildCreated(                          
+        uint date,                               
+        uint data,                               
+        address childAddress                 
+    );                                            
+    function createChild(uint _data) external {  
+        Child child = Child(_data);          
+        children.push(child);                    
+        emit ChildCreated(                       
+            now,                                 
+            _data,                               
+            address(child)                       
+        );                                       
+    }                                            
+}                                                
+                                                 
+contract Child {                                 
+    uint data;                                   
+    constructor(uint _data) public {             
+        data = _data;                            
+    }                                            
+}                                                
+
